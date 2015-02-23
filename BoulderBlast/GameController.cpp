84fb6ec021a2d27f96d53a5c974c8846cca3019f
Tim Guo <timguoqk@gmail.com>
2015-02-23 09:49:05 -0800

    reduce framerate

--- BoulderBlast/GameController.cpp
+++ BoulderBlast/GameController.cpp
@@ -33,7 +33,8 @@ static const double VISIBLE_MIN_Z = -20;
 
 static const double FONT_SCALEDOWN = 760.0;
 
-static const int MS_PER_FRAME = 10;
+//static const int MS_PER_FRAME = 10;
+static const int MS_PER_FRAME = 20;
 
 static const double SCORE_Y = 3.8;
 static const double SCORE_Z = -10;
